a188746e5f24715c66c1105b0a9606c62c23a5cf
nlumiere <nicholaslumiere@gmail.com>
2023-04-07 15:11:44 -0600

    v0.2.0

--- popup.js
+++ popup.js
@@ -8,44 +8,26 @@ function setCustomPlaybackSpeed(speed) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  const playbackRateSlider = document.getElementById("playbackRateSlider");
+  const playbackRateText = document.getElementById("playbackrate");
+
   chrome.storage.local.get(["customPlaybackRate"]).then((result) => {
     let speed = 1;
     if (result) {
       speed = parseFloat(result.customPlaybackRate);
-      document.getElementById("playbackRateSlider").value = speed;
-      document.getElementById("playbackrate").innerHTML =
-        "Playback Speed: " + speed.toString();
+      playbackRateSlider.value = speed;
+      playbackRateText.innerHTML = "Playback Speed: " + speed.toString();
     }
   });
 
-  const ACTION_MESSAGE_LENGTH = 8;
-  document
-    .getElementById("playbackRateSlider")
-    .addEventListener("input", (val) => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const newValue = val.target.value;
-        // const actionStr = "speedSet:" + newValue.toString();
-        // chrome.tabs.sendMessage(tabs[0].id, { action: actionStr });
-        document.getElementById("playbackrate").innerHTML =
-          "Playback Speed: " + newValue.toString();
-        document.addEventListener("mouseup", () => {
-          setCustomPlaybackSpeed(newValue);
-        });
+  playbackRateSlider.addEventListener("input", (val) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const newValue = val.target.value;
+      playbackRateText.innerHTML = "Playback Speed: " + newValue.toString();
+      document.addEventListener("mouseup", () => {
+        setCustomPlaybackSpeed(newValue);
       });
     });
-
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (
-      message.action.length >= ACTION_MESSAGE_LENGTH &&
-      message.action.substring(0, ACTION_MESSAGE_LENGTH) === "buttonClicked"
-    ) {
-      const newValue = parseFloat(
-        message.action.substring(ACTION_MESSAGE_LENGTH + 1)
-      );
-      document.getElementById("playbackRateSlider").value = newValue;
-      document.getElementById("playbackrate").innerHTML =
-        "Playback Speed: " + newValue.toString();
-    }
   });
 
   document.addEventListener("keydown", (event) => {
